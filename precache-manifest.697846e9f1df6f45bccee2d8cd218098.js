4efc8e79b32b727ab564cacb6d0989c43c46e86b
Daniel Rodenberg <drodenberg92@gmail.com>
2023-04-05 15:00:28 -0500

    Updates

---
+++ precache-manifest.697846e9f1df6f45bccee2d8cd218098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35353bf6f5c836c22bcd",
+    "url": "/resume/static/css/main.5d9cb199.chunk.css"
+  },
+  {
+    "revision": "35353bf6f5c836c22bcd",
+    "url": "/resume/static/js/main.35353bf6.chunk.js"
+  },
+  {
+    "revision": "ef7f94a9f7fd294392ec",
+    "url": "/resume/static/js/1.ef7f94a9.chunk.js"
+  },
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "85b804e2e90f1f1f3bf247b14788c48e",
+    "url": "/resume/static/media/danheadshot.85b804e2.jpg"
+  },
+  {
+    "revision": "a0e79270c1a2149d4ab359eddc3a0b91",
+    "url": "/resume/index.html"
+  }
+];
