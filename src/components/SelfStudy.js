6a3ba017b6ae522381d801b2b699b41271f9b4dc
Daniel Rodenberg <drodenberg92@gmail.com>
2023-04-05 16:01:39 -0500

    bullet points

--- src/components/SelfStudy.js
+++ src/components/SelfStudy.js
@@ -14,9 +14,26 @@ class SelfStudy extends Component {
             <div className="col s12 m10 l10">
               <h4>Community</h4>
 
-              <p>Joined a program introducing middle schoolers to code</p>
+
+              <p><strong>Lift For Life Academy</strong></p>
+              <div className="tab">
+                <ul>
+                  <li style={{ listStyleType: 'disc' }}>Volunteer for 1904labs' coders program, teaching middle schoolers to code using MIT's app inventor 2.</li>
+                  <li style={{ listStyleType: 'disc' }}>Lead weekly coding workshops at Lift For Life Academy, empowering students to develop their problem-solving and critical thinking skills.</li>
+                </ul>
+              </div>
+
+              <p><strong>Flutter Health</strong></p>
+              <div className="tab">
+                <ul>
+                  <li style={{ listStyleType: 'disc' }}>Contributing to a flutter library called Flutter Health, helping to develop a tool for tracking health metrics on mobile devices.</li>
+                  <li style={{ listStyleType: 'disc' }}>Collaborate with other developers in the open-source community to improve the functionality and usability of the library.</li>
+                </ul>
+              </div>
+
+              {/* <p>Joined a program introducing middle schoolers to code</p>
               {/* <p>Read Scripture: This is a private project that I recently got invited to. It's a react-native app that is a bible reading plan that has thousands of users.</p> */}
-              <p>Contributed to a flutter library. <a href='https://pub.dev/packages/health' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">flutter health</a> </p>
+              {/* <p>Contributed to a flutter library. <a href='https://pub.dev/packages/health' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">flutter health</a> </p> */}
             </div>
 
           </div>
--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -20,16 +20,20 @@ class WorkHistory extends Component {
               </div>
 
               <p><strong>We are a golf startup working on bringing new and existing golfers to their local courses.</strong></p>
-              <p className="tab">Ran a charity event for Lift For Life Academy in June 2021 that raised over $7,000.
-                Published a mobile app called <a href='https://apps.apple.com/us/app/divvit-golf/id1571822903' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">Divvit Golf</a> using Flutter that is in the app store.
-                Built out, with openlayers and stored in postGIS, over 2000 golf courses throughout the US</p>
+              <div className="tab">
+                <ul>
+                  <li style={{ listStyleType: 'disc' }}>Published a mobile app called <a href='https://apps.apple.com/us/app/divvit-golf/id1571822903' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">Divvit Golf</a> using Flutter that is in the app store.</li>
+                  <li style={{ listStyleType: 'disc' }}>Ran a charity event for Lift For Life Academy in June 2021 that raised over $7,000.</li>
+                  <li style={{ listStyleType: 'disc' }}>Built out, with openlayers and stored in postGIS, over 2000 golf courses throughout the US.</li>
+                </ul>
+              </div>
 
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingRight: 20 }}>
                 <h5>Senior Full Stack Developer @ <a href='https://1904labs.com' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">1904labs</a></h5>
                 <p className="highlight">Oct 2017 - Present</p>
               </div>
 
-              <p><strong>Most recent project was builing a public facing web app for Mastercard.</strong></p>
+              <p><strong>Public-facing Rewards Web Application for Mastercard</strong></p>
               <div className="tab">
                 <ul>
                   <li style={{ listStyleType: 'disc' }}>The application allowed consumers of a bank to see the latest offers and deals that the bank had to offer.</li>
@@ -39,7 +43,7 @@ class WorkHistory extends Component {
               </div>
 
 
-              <p><strong>Another project was working rewriting a data management system at NGA.</strong></p>
+              <p><strong>Complete rewrite of a data management system at NGA.</strong></p>
 
               <div className="tab">
                 <ul>
@@ -47,27 +51,30 @@ class WorkHistory extends Component {
                   <li style={{ listStyleType: 'disc' }}>I was heavily involved in the design process and implementing the design on the front and back end.</li>
                   <li style={{ listStyleType: 'disc' }}>Accomplished setting up the entire application using React/Parcel for the front end and Apollo JS graphql for the back end.</li>
                 </ul>
-
-
-
-
               </div>
 
 
 
-              <p><strong>Initial project at 1904 worked on a front end geospatial web framework for Bayer.</strong></p>
-              <p className="tab">Our framework was being consumed by 11 teams with a number of different use cases. We created an api and ui for developers, to streamline the process of putting together a geospatial web app.
-                One of the features I worked on was an advanced time slider which allows users to look at drone imagery of a field to analyze the different images week by week.
-                Accomplished open sourcing our framework <a href='https://ol-kit.com' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">ol-kit</a>  through Bayer and is the most recognized repo/library</p>
+              <p><strong>React Geospatial Web Framework and Application for Bayer</strong></p>
+              <div className="tab">
+                <ul>
+                  <li style={{ listStyleType: 'disc' }}>Our framework was being consumed by 11 teams with a number of different use cases. We created an api and ui for developers, to streamline the process of putting together a geospatial web app.</li>
+                  <li style={{ listStyleType: 'disc' }}>One of the features I worked on was an advanced time slider which allows users to look at drone imagery of a field to analyze the different images week by week.</li>
+                  <li style={{ listStyleType: 'disc' }}>Accomplished open sourcing our framework <a href='https://ol-kit.com' style={{ color: '#3379b7' }} target='_blank' rel="noopener noreferrer">ol-kit</a>  through Bayer and is the most recognized repo/library</li>
+                </ul>
+              </div>
 
               <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', paddingRight: 20 }}>
                 <h5>Engineer 1 @ Enterprise Holdings</h5>
                 <p className="highlight">Jan 2015 - Sept 2017</p>
               </div>
-              <p><strong>Developed a Spring MVC, Angular web application for internal use called Fleet Planning North America.</strong></p>
-
-              <p className="tab">This application allows three levels of managers to plan their fleet of cars throughout the year.
-                Currently working on a web application for acquiring vehicles for Europe.</p>
+              <p><strong>Developed and maintained a web application using Spring MVC and Angular.</strong></p>
+              <div className="tab">
+                <ul>
+                  <li style={{ listStyleType: 'disc' }}>Fleet Planning North America was used to manage the entire Enterprise fleet across the US.</li>
+                  <li style={{ listStyleType: 'disc' }}>This application allows three levels of managers to plan their fleet of cars throughout the year.</li>
+                </ul>
+              </div>
 
               {/* <h5>Software Intern @ Cerner (Kansas City)</h5>
               <p><span className="highlight">May 2012 - Aug 2014</span></p>
